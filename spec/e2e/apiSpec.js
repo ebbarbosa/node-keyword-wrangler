667007e0e6f627795c1786502065c5b0fea856eb
Eduardo <ebbarbosa@users.noreply.github.com>
2018-01-02 17:21:58 -0200

    first commit

---
+++ spec/e2e/apiSpec.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var request = require('request');
+
+describe('the API', function () {
+    it('should responde to a GET request at /api/keywords/', function (done) {
+
+        var expected = {
+            "_items": [
+                {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+                {'id': 2, 'value': 'Onion', 'categoryID': 1},
+                {'id': 3, 'value': 'Knife', 'categoryID': 2}
+            ]
+        };
+
+        request.get(
+            {
+                'url': 'http://localhost:8080/api/keywords',
+                'json': true
+            },
+            function (err, res, body) {
+                expect(res.statusCode).toBe(200);
+                expect(body).toEqual(expected);
+                done();
+            });
+    });
+});
