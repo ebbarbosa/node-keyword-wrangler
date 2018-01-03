527cf3a4dca68a171e7f31714d5a8b2cdfeb55c7
Eduardo Barbosa <Eduardo.Barbosa@btgpactual.com>
2018-01-03 11:37:53 -0200

    Making the backend recognize different environments

--- src/backend/dbSession.js
+++ src/backend/dbSession.js
@@ -1,7 +1,26 @@
 'use strict'
 
+var env = require('./env.js');
 var DBWrapper = require('node-dbi').DBWrapper;
 
-var dbWrapper = new DBWrapper('sqlite3', { 'path' : 'data/sqlite3/keyword-wrangler.test.sqlite'});
+console.log('Enviroment: %s', env);
+
+var dbOptions = require('../../database.json')[env];
+
+var dbWrapper;
+
+if (dbOptions.driver == 'sqlite3') {
+    dbWrapper = new DBWrapper('sqlite3', { 'path': dbOptions.filename });
+} else if (dbOptions.driver === 'mysql') {
+    dbWrapper = new DBWrapper('mysql', {
+        'host': dbOptions.host,
+        'user': dbOptions.user,
+        'password': dbOptions.password,
+        'database': dbOptions.database
+    });
+} else {
+    throw (new Error('No suitable database config found.'));
+}
+
 dbWrapper.connect();
 module.exports = dbWrapper;
